41ff9d15d4658b49dcf96a9b56d2b9589625b06c
Cao, Jiannan <frogcjn@163.com>
2017-11-14 16:12:27 +0800

    first commit with instructions

---
+++ iPhoneXLockHomeIndicator/iPhoneXLockHomeIndicator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPhoneXLockHomeIndicator
+//
+//  Created by Cao, Jiannan on 2017/11/14.
+//  Copyright © 2017年 Cao, Jiannan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
